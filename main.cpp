4847793dcc630378e3a1335b81c6f5f850383462
Cannondale77 <osullivansg@gmail.com>
2024-08-14 21:26:16 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -12,27 +12,29 @@ Error Message: pinmap not found for peripheral
 Location: 0x4353 Error Value: 0xFFFFFFFF
 */
 #include "mbed.h"
+#include <studio>
 
-
-DigitalOut myled(LED1);
+DigitalOut myled(LED_1);
 Serial pc(USBRX, USBTX);
 Serial dev(p9, p10);
 
 void dev_recv(){
     while(dev.readable()){
+
         pc.putc(dev.getc())    
     }    
 }
 
 void pc_recv(){
     while(pc.readable()){
+        
         dev.putc(pc.getc());    
     }    
 }
 
 int main() {
     pc.baud(9600);
-    device1.baud(115200);
+    dev.baud(9600);
     pc.attach(&pc_recv);
     dev.atach(&dev);
     pc.printf("Hello!! \r\n");
